41aacee02e10165fd43f36d6fe79817fdc668eab
Ramon Carl Conda <ramoncarlconda726@gmail.com>
2026-02-06 21:29:11 +0800

    try making landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Counter from "@/components/Counter";
+import Link from "next/link";
 
 async function getPosts() {
   const res = await fetch("http://localhost:3000/api/posts");
@@ -9,13 +10,21 @@ export default async function Home() {
   const posts = await getPosts();
 
   return (
-    <main className="p-8">
-      <h1 className="text-xl font-bold mb-4">Posts</h1>
-      <ul>
+    <main className="p-8 flex flex-col justify-center h-screen">
+      {/* <h1 className="text-xl font-bold mb-4">Posts</h1> */}
+      {/* <ul>
         {posts.map((post: any) => (
           <li key={post.id}>{post.title}</li>
         ))}
-      </ul>
+      </ul> */}
+
+      <div className="flex flex-col items-center md:items-start gap-2 md:w-1/2 ">
+        <h1 className="text-4xl font-bold">Landing Page</h1>
+        <p className="text-center md:text-start">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos consequuntur sequi quo placeat quia nobis asperiores excepturi, harum beatae, similique impedit minima quibusdam non corporis eum amet. Nostrum, doloribus dicta?
+        </p>
+        <Link href="/" className="p-2 px-8 rounded-xl bg-blue-500 font-bold cursor-pointer">More Info</Link>
+      </div>
     </main>
   );
 }
