9184945b6679c32e2007b662800406f7bb834420
Ramon Carl Conda <ramoncarlconda726@gmail.com>
2026-02-06 09:47:42 +0800

    practice tailwindcss flex, grid and a main style for multiple elements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,6 +12,8 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const cardsStyle = "bg-white text-black p-8 rounded-md ";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,6 +30,41 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="flex justify-center flex-col items-center p-8 border-0 m-8 rounded-xl bg-white text-black">
+          <strong className="">My OJT App</strong>
+          <Link href="/">Home</Link>
+          <Link href="/about">About</Link>
+        </header>
+        <div className="flex flex-col flex-wrap justify-center h-auto gap-2 m-8 p-8  border-2 rounded-xl md:flex-row">
+          <div className="bg-white text-black p-8 rounded-md ">Lorem </div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 m-8 p-8 border-2 rounded-xl text-sm md:text-lg lg:text-xl">
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+          <div className="bg-white text-black p-8 rounded-md ">lorem</div>
+        </div>
+        <h1 className="text-center text-2xl font-bold">Uses same styles on multiple elements using constant class string</h1>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-2 m-8 p-8 border-2 rounded-xl text-sm md:text-lg lg:text-xl">
+          <div className={cardsStyle}>Card 1</div>
+          <div className={cardsStyle}>Card 2</div>
+          <div className={cardsStyle}>Card 3</div>
+        </div>
         {children}
       </body>
     </html>
